020cc831443dd3689087da23f33b8b5149311ec8
Mahsa <mahsa.nadery69@gmail.com>
2025-08-06 18:50:16 +0330

    the part of JS navbar is done

--- script.js
+++ script.js
@@ -1,66 +1,103 @@
-const validEmail = (email)=> {
-    const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return pattern.test(email);
-}
+const validEmail = (email) => {
+  const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return pattern.test(email);
+};
 const loginForm = document.getElementById("loginForm");
-if(loginForm) {
-    loginForm.addEventListener('submit', function(e){
-        e.preventDefault();
-        const email = document.getElementById('email').value.trim();
-        const password =  document.getElementById('password').value.trim();
-        if(!validEmail(email)) {
-            alert("The format of Email is not valid!!");
-            return
-        }
-        let users = JSON.parse(localStorage.getItem('users')) || [];
-        const existUser = users.find(user => user.email === email);
-        if(existUser) {
-            if(existUser.password !== password) {
-                alert("password is incorrect!");
-                return
-            }
-            localStorage.setItem('currentUser', JSON.stringify(existUser));
-            alert(`Welcome To The Transaction Budget Dear ${existUser.email}`);
-            window.location.href = "index.html";
-        } else {
-            alert("No account found with this email!, please sign up");
-            return
-        }
-
-    });
+if (loginForm) {
+  loginForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+    if (!validEmail(email)) {
+      alert("The format of Email is not valid!!");
+      return;
+    }
+    let users = JSON.parse(localStorage.getItem("users")) || [];
+    const existUser = users.find((user) => user.email === email);
+    if (existUser) {
+      if (existUser.password !== password) {
+        alert("password is incorrect!");
+        return;
+      }
+      localStorage.setItem("currentUser", JSON.stringify(existUser));
+      alert(`Welcome To The Transaction Budget Dear ${existUser.email}`);
+      window.location.href = "index.html";
+    } else {
+      alert("No account found with this email!, please sign up");
+      return;
+    }
+  });
 }
 
 const registerForm = document.getElementById("registerForm");
 if (registerForm) {
-    registerForm.addEventListener('submit', function(e){
-        e.preventDefault();
-        const name= document.getElementById('name').value;
-        const email = document.getElementById('email').value;
-        const password =  document.getElementById('password').value;
-        const confirmPassword = document.getElementById("confirmPassword").value;
+  registerForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirmPassword").value;
 
-        if (confirmPassword !== password) {
-            alert("The password is not match!");
-            return
-        }
-               
-        else if(!validEmail(email)) {
-            alert("The format of Email is not valid!!");
-            return
-        } 
-        
-        let users = JSON.parse(localStorage.getItem('users')) || [];
-        const existUser = users.find(user => user.email === email)
-        if (existUser) {
-            alert("This Email is already used!");
-            return
-        } 
-        const user = {name, email, password};
-        users.push(user);    
-        localStorage.setItem('users', JSON.stringify(users));
-        alert("Registeration is Successfully :))");
-        window.location.href = "login.html";
+    if (confirmPassword !== password) {
+      alert("The password is not match!");
+      return;
+    } else if (!validEmail(email)) {
+      alert("The format of Email is not valid!!");
+      return;
+    }
 
-    });
+    let users = JSON.parse(localStorage.getItem("users")) || [];
+    const existUser = users.find((user) => user.email === email);
+    if (existUser) {
+      alert("This Email is already used!");
+      return;
+    }
+    const user = { name, email, password };
+    users.push(user);
+    localStorage.setItem("users", JSON.stringify(users));
+    alert("Registeration is Successfully :))");
+    window.location.href = "login.html";
+  });
 }
 
+const formatDate = (date) => {
+  const options = { month: "short", day: "2-digit", year: "numeric" };
+  return new Intl.DateTimeFormat("en-US", options).format(date);
+};
+
+const greeting = document.getElementById("greeting");
+const loginBtn = document.getElementById("loginButton");
+const logoutBtn = document.getElementById("logoutButton");
+
+document.addEventListener("DOMContentLoaded", function () {
+  const today = new Date();
+  const formattedDate = formatDate(today);
+  const currentDate = document.getElementById("currentDate");
+  if (currentDate) {
+    currentDate.innerHTML = formattedDate;
+  }
+
+  let loginUser = JSON.parse(localStorage.getItem("currentUser"));
+
+  
+  if (loginUser) {
+    greeting.innerHTML = `Hello, ${loginUser.name}`;
+    logoutBtn.classList.remove("hidden");
+    loginBtn.classList.add("hidden");
+    if (logoutBtn) {
+        logoutBtn.addEventListener("click", function () {
+            localStorage.removeItem("currentUser");
+            location.reload();
+      });
+    }
+  } else {
+    logoutBtn.classList.add("hidden");
+    greeting.innerHTML = `Hello, please`;
+    if(loginBtn) {
+        loginBtn.addEventListener("click", function () {
+            window.location.href = "login.html";
+        });
+    }
+  }
+ 
+});
