0a219aed67ba85aca3dcc1eacef062a3cb7c87e6
Mahsa <mahsa.nadery69@gmail.com>
2025-08-11 15:40:58 +0330

    show the List of transaction for each user

--- script.js
+++ script.js
@@ -78,8 +78,6 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   let loginUser = JSON.parse(localStorage.getItem("currentUser"));
-
-  
   if (loginUser) {
     greeting.innerHTML = `Hello, ${loginUser.name}`;
     logoutBtn.classList.remove("hidden");
@@ -99,40 +97,91 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
   }
- 
+  const transactions = getUserTransaction();
+  renderTransactions(transactions);
+
 });
 const transactionForm = document.getElementById("transactionForm");
 transactionForm.addEventListener('submit', function(e){
-    e.preventDefault();
-    const title = document.getElementById("title").value.trim();
-    const amount = document.getElementById("amount").value.trim();
-    const type = document.getElementById("type").value;
-    const category = document.getElementById("category").value;
-    const date = document.getElementById("date").value;
+  e.preventDefault();
+  const title = document.getElementById("title").value.trim();
+  const amount = document.getElementById("amount").value.trim();
+  const type = document.getElementById("type").value;
+  const category = document.getElementById("category").value;
+  const date = document.getElementById("date").value;
 
-    let currentUser = JSON.parse(localStorage.getItem("currentUser"));
-    let customAmount = Math.abs(Number(amount));
-    if(type === "expense") {
-        customAmount = -customAmount;
-    }
-    
-    if (currentUser) {
-        const transactionKey = `transactions_${currentUser.email}`;
-        let transactions = JSON.parse(localStorage.getItem(transactionKey)) || [];
-        const transaction = {
-          id: Date.now(),
-          title,
-          amount: customAmount,
-          type,
-          category,
-          date,
-          cssClass: type,
-        };
-        transactions.push(transaction);
-        localStorage.setItem(transactionKey, JSON.stringify(transactions));
-    } else {
-        alert("Please, first login");
-        return
-    }
-    transactionForm.reset();
+  let currentUser = JSON.parse(localStorage.getItem("currentUser"));
+  let customAmount = Math.abs(Number(amount));
+  if(type === "expense") {
+      customAmount = -customAmount;
+  }
+  
+  if (currentUser) {
+      const transactionKey = `transactions_${currentUser.email}`;
+      let transactions = JSON.parse(localStorage.getItem(transactionKey)) || [];
+      const transaction = {
+        id: Date.now(),
+        title,
+        amount: customAmount,
+        type,
+        category,
+        date,
+        cssClass: type,
+      };
+      transactions.push(transaction);
+      localStorage.setItem(transactionKey, JSON.stringify(transactions));
+      
+  } else {
+      alert("Please, first login");
+      return
+  }
+  transactionForm.reset(); 
+  const transactions = getUserTransaction();
+  renderTransactions(transactions);
 });
+
+function getUserTransaction() {
+  let currentUser = JSON.parse(localStorage.getItem("currentUser"));
+  let transactions = [];
+  if(currentUser) {
+    const transactionKey = `transactions_${currentUser.email}`;
+    transactions = JSON.parse(localStorage.getItem(transactionKey)) || [];
+  } 
+  return transactions;
+}
+
+function renderTransactions(list) {
+  const transactionList = document.getElementById("transactionList");
+  if (!transactionList) {
+    return;
+  } 
+  if (list.length === 0) {
+    transactionList.innerHTML = "<p>Transaction Lists are Empty</p>";
+    return;
+  }
+  transactionList.innerHTML = "";
+  list.forEach(item => {
+    const newList = document.createElement('div');
+    newList.className = `transaction-container ${item.type}`;
+    newList.innerHTML = `
+      <div class="budget-title">
+          <h2>${item.type}: ${item.title}</h2>
+      </div>
+      <div class="budget-detail">  
+          <p><strong>Category: </strong>${item.category}</p>
+          <h3>${item.amount}$</h3>         
+      </div>
+      <div class="budget-date">
+          <h5>${item.date}</h5>
+          <button type="button" id="deleteBtn" class="deleteBtn" data-id="${item.id}">Delete</button>
+      </div>`;
+    transactionList.append(newList);
+    deleteTransaction();
+    
+  });
+}
+
+
+
+
+ 
